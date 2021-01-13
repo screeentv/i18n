dced1018aa0cc77d08904c236b17079ea99aad81
N U R O ™ <me@nuro.dev>
2021-01-13 19:19:43 +0000

    Added initial project structure

---
+++ src/en/index.js
@@ -0,0 +1,5 @@
+export const messages = {
+	
+};
+
+export default messages;
---
+++ src/index.js
@@ -0,0 +1,7 @@
+import { messages as en } from './en';
+
+export const messages = {
+	en,
+}
+
+export default messages;
